72915822d3e442b3ce6e9153f7c33b256bd68b61
Alex Chambers_Jones <alexcj2802@gmail.com>
2021-10-10 13:50:31 +0100

    WORKSWORKSWORKSOWRKSOWRKSOWKOKDbdsfkhdfsjkfkjdshfkjs

--- proxyClient.py
+++ proxyClient.py
@@ -10,36 +10,34 @@ HOST = "0.0.0.0"
 PORT = 1085
 BUFFERSIZE = 4096
 
-client = httpSocketClient.HttpSocketClient(REMOTE)
-
-client.connect()
-
-
-def loop_local_to_remote(local_sock, host):
+def loop_local_to_remote(local_sock, host, client):
     while True:
-        data = local_sock.recv(BUFFERSIZE)
-        print("\n\n\n\n\nLOCAL_TO_REMOTE: " + str(data))
+        try:
+            data = local_sock.recv(BUFFERSIZE)
+        except ConnectionResetError:
+            print("CONNECTION RESET")
+            return
+
+        if len(data) == 0:
+            print("EMPTY MESSAGE ERR")
+            return
+
         client.send(shared.encrypt_and_encode(host, data))
 
 
-def loop_remote_to_local(local_sock, host):
+def loop_remote_to_local(local_sock, client):
     while True:
-        data = shared.str_to_bytes(client.receive())
-        print("\n\n\n\n\nREMOTE_TO_LOCAL: " + str(data))
-        local_sock.send(data)
-
-
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    while True:
+        data = shared.decrypt_and_decode_reply(client.receive())
         try:
-            s.bind((HOST, PORT))
-            print("Bound at " + str(PORT))
-            break
-        except:
-            PORT += 1
+            local_sock.send(data)
+        except ConnectionResetError:
+            print("CONNECTION RESET")
+            return
 
-    s.listen()
-    conn, addr = s.accept()
+def handle_conn(conn):
+    client = httpSocketClient.HttpSocketClient(REMOTE)
+
+    client.connect()
 
     initial_data = conn.recv(BUFFERSIZE)
 
@@ -55,8 +53,24 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
 
     print("Responded")
 
-    Thread(target=loop_local_to_remote, args=(conn, host)).start()
-    Thread(target=loop_remote_to_local, args=(conn, host)).start()
+    Thread(target=loop_local_to_remote, args=(conn, host, client)).start()
+    Thread(target=loop_remote_to_local, args=(conn, client)).start()
 
     while True:
         time.sleep(1)
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    while True:
+        try:
+            s.bind((HOST, PORT))
+            print("Bound at " + str(PORT))
+            break
+        except:
+            PORT += 1
+
+    while True:
+        s.listen()
+
+        conn, addr = s.accept()
+
+        Thread(target=handle_conn, args=(conn,)).start()
--- proxyServer.py
+++ proxyServer.py
@@ -1,6 +1,8 @@
 import httpSocketServer
 import socket
+from threading import *
 import shared
+import time
 
 BUFFERSIZE = 4096
 
@@ -17,15 +19,36 @@ def new_client(headers):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect((HOST, PORT))
 
-        while True:
-            print("Sending to remote (fetched from client): " + str(data))
-            s.send(data)
-            resp = s.recv(BUFFERSIZE)
-            print("---------------------")
-            print("Got from remote, echoing back: " + str(resp))
-            server.send(_id, shared.encrypt_and_encode_reply(resp))
-            print("Echoed back")
+        def outwards_proxy(_id, s):
+            print("Outwards proxy started")
+            while True:
+                data, host = shared.decrypt_and_decode(server.receive(_id))
+                if len(data) == 0:
+                    print("ERR: DATA TO SHORT: ABORT")
+                    break
 
-            data, host = shared.decrypt_and_decode(server.receive(_id))
+                print("Sending data from client to remote: " + str(data))
+                s.send(data)
+
+        def inwards_proxy(_id, s):
+            print("Inwards proxy started")
+            while True:
+                resp = s.recv(BUFFERSIZE)
+                if len(resp) == 0:
+                    print("ERR: DATA TO SHORT: ABORT")
+                    break
+
+                print("Sending data from remote to client: " + str(resp))
+                server.send(_id, shared.encrypt_and_encode_reply(resp))
+
+        Thread(target=inwards_proxy, args=(_id, s)).start()
+        Thread(target=outwards_proxy, args=(_id, s)).start()
+
+        #got this earlier; we have to send this along
+        s.send(data)
+
+        while True:
+            time.sleep(1)
+
 
 server = httpSocketServer.HttpSocketServer("0.0.0.0", 1082, new_client)
